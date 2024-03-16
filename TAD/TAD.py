68b63a3a5d4ef622b3bf90dd08f474fe1d37d834
Bruno Ilha Carneiro <126731354+Brunoilha@users.noreply.github.com>
2024-03-16 17:16:06 -0300

    Update TAD.py

--- TAD/TAD.py
+++ TAD/TAD.py
@@ -3,15 +3,13 @@ class lista:
         self.info = info
         self.prox = None
 
-
-def cria_lista():
+def lista_cria():
     return lista()
 
-
-def insere_lista(lst,valor):
-    novo_no = lista(valor)
-    novo_no.prox = lst
-    return novo_no
+def lista_insere(lst, valor):
+    novo = lista(valor)
+    novo.prox = lst
+    return novo
 
 def lista_imprime(lst):
     atual = lst
@@ -30,19 +28,52 @@ def lista_busca(lst,valor):
         atual = atual.prox
     return False
 
+def lista_retira(lst,valor):
+    novo_x = lista(valor)
+    novo_x.prox = lst
+    lst = lst.prox
+    return novo_x
 
-lst = cria_lista()
+    aux = lst
+    if lst.info == valor:
+        aux = lst.prox
+        atual.prox = atual.prox.prox
+        aux = None
+
+    aux = lst
+    atual = lst.prox
+    while atual is not None:
+        if atual.info == valor:
+            aux.prox = atual.prox
+            atual.prox = None
+            return lst
+        aux = atual
+        atual = atual.prox
+
+
+lst = lista_cria()
 print(lista_vazia(lst))
-lst = insere_lista(lst,50)
-lst = insere_lista(lst,60)
-lst = insere_lista(lst,70)
-lst = insere_lista(lst,80)
-lista_imprime(lst)
+lst = lista_insere(lst,50)
+lst = lista_insere(lst,20)
+lst = lista_insere(lst,90)
+lst = lista_insere(lst,30)
+lst = lista_insere(lst,250)
+lista_imprime((lst))
 lista_vazia(lst)
+lst = lista_retira(lst, 20)
 
-
-busca = lista_busca(lst,50)
+busca = lista_busca(lst, 50)
 if busca:
-    print("achei {} " .format(busca.info))
+    print("achei {}".format((busca.info)))
 else:
     print("nao achei")
+
+retira = lista_retira(lst,20)
+
+if retira:
+    print("retirei {}".format((retira.info)))
+
+else:
+    print("nao encontrado")
+
+
