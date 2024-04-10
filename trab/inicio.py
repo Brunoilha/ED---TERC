f8e2a0cca35ae896552987cfe17b4579b15cbc0e
Bruno Ilha Carneiro <126731354+Brunoilha@users.noreply.github.com>
2024-04-09 22:10:28 -0300

    Update inicio.py

--- trab/inicio.py
+++ trab/inicio.py
@@ -1,68 +1,43 @@
+
 class Pilha:
     def __init__(self, tam=20):
         self.tam = tam
         self.n = 0
         self.vet = [None] * tam
 
-
 def pilha_cria():
     return Pilha()
 
-def pilha_push(p, o):
+def pilha_push(p, num, operadores):
     if p.n < p.tam:
-        p.vet[p.n] = o
+        p.vet[p.n] = num
         p.n += 1
-        print("numero", o, "add")
-    else:
-        print("pilha cheia")
+        print("numero", num, "add")
 
-def pilha_cria_operador():
-    return Pilha()
-
-
-
-def pilha_pop(p):
-    if p.n > 0:
-        o = p.vet[p.n - 1 ]
-        print("retirado da pilha", o)
-    else:
-        print("numero inexistente")
-    return o
-
-def pilha_push_operador(p,operadores):
-    if p.n < p.tam:
+        p.n < p.tam
         p.vet[p.n] = operadores
         p.n += 1
         print("operador", operadores, "foi add")
     else:
-        print("nao foi possivel add")
+        print("operação salva")
 
-
-
+## importação
 from calculadora import *
 
 if __name__ == "__main__":
+
     p = pilha_cria()
-    print("criando pilha")
+    
+    while True:
+        num = int(input("digite um numero: "))
+        operadores = input("digite os operandos: ")
+        if num == -1 and operadores == -1:
+            break
+        pilha_push(p, num, operadores)
+        print(p.n)
+        print(p)
+        
+    
+    
 
-    pilha_push(p, 5)
-
-
-    pilha_push(p, 15)
-
-
-    pilha_pop(p)
-
-
-    p = pilha_cria_operador()
-    pilha_push_operador(p, "/")
-
-
-
-
-
-
-
-
-
-
+    
